6f61af7320a3d27b735040281bead5bda2938aed
JOSUE BONILLA CARDENAS <josuebc2720@gmail.com>
2022-10-03 20:21:08 -0500

    03/oct/2022

--- Convertidor/Source.cpp
+++ Convertidor/Source.cpp
@@ -1,3 +1,12 @@
+/**
+	Progaramacion oreintada a objetos
+	Tarea 3
+	Josue Bonilla Cárdenas
+	Reg:22110106
+	CETI colomos
+	03/Octubre/2022
+**/
+
 #include<iostream>
 #include<stdlib.h>
 #include"Correr.h"
